f319be2d0b24c231d09f5b6c2606217917fef4ec
Vinicius Holanda <holanda796@gmail.com>
2020-06-18 12:40:41 -0300

    adicionado API MailChimp

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const https = require("https")
 
 //necessário para renderizar o html no localhost
 const bodyParser = require("body-parser")
+const { request } = require('http')
 
 //necessário para renderizar o html no localhost
 app.use(bodyParser.urlencoded({extended: true}))
@@ -18,15 +20,52 @@ app.use(express.static("public"))
 
 app.post('/', function (req, res) {
 
-    let firstName = req.body.fName
-    let lastName = req.body.lName
-    let emailName = req.body.email
+    const firstName = req.body.fName
+    const lastName = req.body.lName
+    const emailName = req.body.email
+
+    const data = {
+      members: [
+        {
+          email_address: emailName,
+          status: "subscribed",
+          merge_fields: {
+            FNAME: firstName,
+            LNAME: lastName
+
+          }
+        }
+      ]
+    };
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us10.api.mailchimp.com/3.0/lists/5cbcf1026f";
+
+    const options = {
+      method: "POST",
+      auth: "vinicius1:0632946a61434188c8628af8a98125a3-us10"
+    }
+
+    const request = https.request(url, options, function(response){
+      response.on("data", function(data){
+        console.log(JSON.parse(data));
+        
+      })
+    })
+
+    request.write(jsonData);
+    request.end();
+
  
-    console.log(firstName, lastName, emailName);
+  
     
 })
 
+//Api Key
+//0632946a61434188c8628af8a98125a3-us10
 
+//List ID
+//5cbcf1026f
 
 
 
